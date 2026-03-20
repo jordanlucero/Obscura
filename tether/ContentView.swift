cd3d7f178d55a7be30fcfb5424ac1a67ecf2962d
jordanlucero <132685300+jordanlucero@users.noreply.github.com>
2026-03-20 17:29:57 -0500

    Initial Commit

---
+++ tether/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tether
+//
+//  Created by Jordan Lucero on 3/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tether/tetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  tetherApp.swift
+//  tether
+//
+//  Created by Jordan Lucero on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct tetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
